747a520706f7cc0d86106ec8a223609997905571
Ashwani Gupta <ashwanigupta125@gmail.com>
2018-05-21 23:26:19 +0530

    Add data cache

--- currency-converter/public/sw.js
+++ currency-converter/public/sw.js
@@ -1,12 +1,16 @@
-let staticCacheName = 'cc-static-v2';
-let flagsCache = 'cc-currency-flags';
+let staticCacheName = 'cc-static-v1';
+let flagsCache = 'cc-currency-flags-v1';
+let dataCache = 'cc-data-v1';
+
 let allCaches = [
   staticCacheName,
-  flagsCache
+  flagsCache,
+  dataCache
 ];
 
 let urlsToCache = [
   '/',
+  '/rates',
   '/js/init.js',
   '/js/index.js',
   '/js/materialize.js',
@@ -47,14 +51,56 @@ self.addEventListener('waiting', function() {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[ServiceWorker] Fetch', event.request.url);
 
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      console.log(response);
+  const url = new URL(event.request.url);
+
+  // cache material icons
+  if (url.hostname === 'fonts.gstatic.com') {
+    caches.match(url.href).then(function(response) {
       return response || fetch(event.request);
     })
-  );
+  }
+
+  // cache data
+  else if (url.pathname.startsWith('/api/')) {
+    event.respondWith(
+      caches.open(dataCache).then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        }).catch(function() {
+          if (url.pathname.startsWith('/api/convert')) {
+            let baseCurrency = url.searchParams.get('from');
+            let quoteCurrency = url.searchParams.get('to');
+            let amount = url.searchParams.get('amount');
+            return caches.match(`/api/rates?base=${baseCurrency}`)
+              .then(function(response) {
+                console.log(`${url.protocol}//${url.host}/api/rates?base=${baseCurrency}`);
+                let convertedAmount = response.rates[quoteCurrency] * amount;
+                return {time: response.time, amount: convertedAmount.toString()}
+              })  
+          }
+          return caches.match(event.request.url).then(function(response) {
+            if (response) return response;
+            // return 'NIL';
+          });
+        });
+      })
+    );
+  }
+
+  // cache and serve flags
+  // else if (url.pathname.startsWith('/flags/')) {
+  //   event.respondWith(serveFlag(event.request));
+  // }
+
+  else {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
 
 
@@ -62,15 +108,18 @@ function serveFlag(request) {
 
   return caches.open(flagsCache).then(function(cache) {
     return cache.match(request.url).then(function(response) {
-      if (response) return response;
+      if (response) {
+        return response;
+      }
 
       return fetch(request).then(function(networkResponse) {
+        if (!response) {
+          return caches.match('/flags/placeholder.png').then(function(placeholderResponse) {
+            return placeholderResponse;
+          })
+        }
         cache.put(request.url, networkResponse.clone());
         return networkResponse;
-      }).catch(function() {
-        return caches.match('/flags/placeholder.png').then(function(placeholderResponse) {
-          return placeholderResponse;
-        })
       });
     });
   });
